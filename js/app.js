61285a26f923a388fb86053cfe271d300202dedf
olandami123 <olanoyedamilare@gmail.com>
2024-01-17 13:04:45 +0100

    victor commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        smartSpeed: 1000,
+        dots:false,
+        navText:['PREV','NEXT'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+               
+            },
+            1000:{
+                
+            }
+        }
+    })
+    
+    });
